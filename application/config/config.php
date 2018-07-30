7d2da3046fcaa6a520355625f4387396f66891e8
azhadi76 <41886481+azhadi76@users.noreply.github.com>
2018-07-30 17:25:06 +0800

    update config base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'cifirst.herokuapp.com/ciblog';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
