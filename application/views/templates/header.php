b841f5bcb24996f9f1bc6695e329b0df4bd3f032
azhadi76 <41886481+azhadi76@users.noreply.github.com>
2018-07-30 17:36:13 +0800

    edit header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,20 +13,20 @@
         </div>
         <div id="navbar">
           <ul class="nav navbar-nav">
-            <li><a href="<?php echo base_url(); ?>">Home</a></li>
-            <li><a href="<?php echo base_url(); ?>about">About</a></li>
-             <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
-             <li><a href="<?php echo base_url(); ?>categories">Categories</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+             <li><a href="/posts">Blog</a></li>
+             <li><a href="/categories">Categories</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
           <?php if(!$this->session->userdata('logged_in')) : ?>
-            <li><a href="<?php echo base_url(); ?>users/login">Login</a></li>
-            <li><a href="<?php echo base_url(); ?>users/register">Register</a></li>
+            <li><a href="/users/login">Login</a></li>
+            <li><a href="/users/register">Register</a></li>
           <?php endif; ?>
           <?php if($this->session->userdata('logged_in')) : ?>
-            <li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
-            <li><a href="<?php echo base_url(); ?>categories/create">Create Category</a></li>
-            <li><a href="<?php echo base_url(); ?>users/logout">Logout</a></li>
+            <li><a href="/posts/create">Create Post</a></li>
+            <li><a href="/categories/create">Create Category</a></li>
+            <li><a href="/users/logout">Logout</a></li>
           <?php endif; ?>
           </ul>
         </div>
